a9c70e0974171b94e55d049fb65cce61dd090055
Emin Panahov <117656930+es589310@users.noreply.github.com>
2025-01-31 00:01:51 +0400

    Delete author-service directory

--- author-service/src/main/java/com/example/authorservicef/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.authorservicef.exception;
-
-public class AuthorNotFoundException extends RuntimeException{
-    public AuthorNotFoundException(String message) {
-        super(message);
-    }
-}
--- author-service/src/main/java/com/example/authorservicef/request/AuthorRequest.java
+++
@@ -1,5 +0,0 @@
-package com.example.authorservicef.request;
-
-
-public record AuthorRequest(String name, String biography, String birthDate) {
-}
