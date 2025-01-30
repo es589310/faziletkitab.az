85d0d2aed29e33549d06bb050d3ed1d583237147
mr.panahov <panahovemin10@gmail.com>
2025-01-31 01:16:19 +0400

    Catalog yenidən yaradıldı

---
+++ catalog-service/src/main/java/com/example/catalogservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.catalogservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Xüsusi xətalar üçün bir handler
+    @ExceptionHandler(CatalogNotFoundException.class)
+    public ResponseEntity<String> handleCatalogNotFoundException(CatalogNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Ümumi xətalar üçün bir handler
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+
+}
---
+++ catalog-service/src/main/java/com/example/catalogservice/response/ImageResponse.java
@@ -0,0 +1,10 @@
+package com.example.catalogservice.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImageResponse {
+    private String imageUrl;
+}
