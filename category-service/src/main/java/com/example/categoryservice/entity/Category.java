891abe86a7671bebdda0c3e0c7b7522b36bb6f30
mr.panahov <panahovemin10@gmail.com>
2025-03-14 16:27:14 +0400

    search üçün son işlər

--- category-service/src/main/java/com/example/categoryservice/entity/Category.java
+++ category-service/src/main/java/com/example/categoryservice/entity/Category.java
@@ -15,9 +15,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @Table(name = "categories")
 public class Category {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
     private String categoryDescription;
+
 }
